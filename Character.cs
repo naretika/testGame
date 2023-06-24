20bcf72385cea7f3e585226d750366282c712eb1
naretika <Ekaterina.Kalau98@gmail.com>
2023-06-24 22:51:10 +0300

    start with OOP and Unity

--- Character.cs
+++ Character.cs
@@ -13,15 +13,48 @@ public class Character
         name = "Not Assigned";
     }
 
-    public void PrintStatsInfo()
+    public virtual void PrintStatsInfo()
     {
-        Debug.LogFormat("hero:{0} - {1} EXP", name, exp);
+        Debug.LogFormat("Hero:{0} - {1} EXP", name, exp);
     }
 
     public Character (string name)
     {
         this.name = name;
     }
+
+    private void Reset () 
+    {
+        this.name = "not assigned";
+         this.exp = 0;
+    }
+}
+
+public class Paladin: Character
+{
+    public int age;
+    public Weapon weapon;
+    public Paladin(string name, int age, Weapon weapon) : base(name)
+    {
+        this.age = age;
+        this.weapon = weapon;
+    }
+
+    public Paladin(string name) : base(name)
+    {
+
+    }
+    public void PrintAge()
+    {
+        Debug.LogFormat("Paladin age:{0} - {1} years", name, age);
+    }
+
+    public override void PrintStatsInfo()
+    {
+        Debug.LogFormat("Hail {0} - take your {1}!", name, weapon.name);
+    }
+
+
 }
 
 public struct Weapon
--- LearningCurve.cs
+++ LearningCurve.cs
@@ -4,21 +4,57 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    
+    private Transform camTransform;
+    private Transform lightTransform;
+    public GameObject directionLight;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //Transform camTransform = GetComponent<Transform>();
+        camTransform = this.GetComponent<Transform>();
+        Debug.Log(camTransform.localPosition);
+
+        //directionLight = GameObject.Find("Diretional Light");
+        lightTransform = directionLight.GetComponent<Transform>();
+        Debug.Log(lightTransform.localPosition);
+
+        /*
         Character hero = new Character();
         hero.PrintStatsInfo();
 
         Character heroine = new Character("Agatha");
         heroine.PrintStatsInfo();
-
+// это структура, она не хранит ссылки
         Weapon weapon = new Weapon("Hounting Bow", 105);
+        Weapon hountingBow = new Weapon("Hounting Bow", 105);
+        Weapon warBow = hountingBow;
+        hountingBow.PrintWeaponStats();
+        warBow.PrintWeaponStats();
+        warBow.name = "pushka";
+        warBow.damage = 50;
+        warBow.PrintWeaponStats();
+        hountingBow.PrintWeaponStats();
 
+
+// А это класс, оперирует ссылками, а так, та же петрушка, что и структура, но значения поменяются в обеих ячейках
         Character hero2 = hero;
+        hero2.name = "Torvik";
         hero2.PrintStatsInfo();
+        hero.PrintStatsInfo();
+
+        //hero2.Reset();
+
+        //тестирование наследования классов
+
+        Paladin knight = new Paladin("Sir Arthur", 34, hountingBow);
+        knight.exp = 20;
+        knight.PrintStatsInfo();
+        knight.PrintAge();
+*/
     }
+
 
     // Update is called once per frame
     void Update()
